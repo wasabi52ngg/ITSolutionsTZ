65490582798fa36198539898399735b779c011b9
wasabi52ngg <vovasharafutdinov@mail.ru>
2025-08-25 17:37:32 +0300

    Fixed urls

--- ITSolutionsTZ/urls.py
+++ ITSolutionsTZ/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('quotes/admin/', admin.site.urls),
     path('quotes/', include('quotes.urls')),
 ]
